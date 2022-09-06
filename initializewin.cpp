1a7690f64d5ecc2bec71381b330a16f58514f0ea
JuicyActive01Gilbert <810242728@qq.com>
2022-09-06 11:24:33 +0800

    添加打赏相关图片，谢谢各位~

--- initializewin.cpp
+++ initializewin.cpp
@@ -1,6 +1,7 @@
 #include "initializewin.h"
 #include <QPainter>
 #include <QImage>
+#include <QPixmap>
 
 InitializeWin::InitializeWin(QWidget *parent)
     : QWidget(parent)
@@ -19,11 +20,19 @@ void InitializeWin::initialize()
 {
     m_pLabTitle->setProperty("LabelType","Title");
     m_pLabTitle2->setProperty("LabelType","Title");
-//    label_5->setProperty("LabelType","Title");
+    //    label_5->setProperty("LabelType","Title");
     m_pBtnStart->setProperty("ButtonColor","MainBtn");
     m_pBtnQuit->setProperty("ButtonColor","MainBtn");
 
-    this->setWindowTitle(tr("《觅长生》Mod制作v2.0.0"));
+    this->setWindowTitle(tr("《觅长生》Mod制作v2.0.1"));
+
+    QImage img(":/resources/daShang/wx.png");
+    img = img.scaled(220,260);
+    m_pLabWX->setPixmap(QPixmap::fromImage(img));
+
+    img = QImage(":/resources/daShang/zfb.png");
+    img = img.scaled(220,260);
+    m_pLabZFB->setPixmap(QPixmap::fromImage(img));
 
     connect(m_pBtnStart,SIGNAL(clicked(bool)),this,SIGNAL(startMake()));
     connect(m_pBtnQuit,SIGNAL(clicked(bool)),this,SIGNAL(quitMake()));
